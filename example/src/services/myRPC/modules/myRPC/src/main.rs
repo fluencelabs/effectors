f8a3b21ab45fcacaacee7c0dd68ed8cab16f20cd
Maria Kuklina <maria@fluence.one>
2024-02-29 12:59:30 +0100

    update example

--- example/src/services/myRPC/modules/myRPC/src/main.rs
+++ example/src/services/myRPC/modules/myRPC/src/main.rs
@@ -6,17 +6,47 @@ use marine_rs_sdk::module_manifest;
 
 use curl_effector_imports as curl;
 use curl_effector_imports::CurlRequest;
+
+use ipfs_effector_imports as ipfs;
+
 module_manifest!();
 
 pub fn main() {}
 
 #[marine]
-pub fn greeting(name: String) -> String {
+pub fn simple_get_http(url: String) -> String {
+    let path = vault_path("some_path");
     let my_request = CurlRequest {
-        url: "my_url".to_string(),
+        url,
         headers: vec![],
-        output_vault_path: "some path".to_string()
+        output_vault_path: path.clone(),
     };
     let result = curl::curl_get(my_request);
-    format!("Hi, {}", name)
+    if result.success {
+        match std::fs::read_to_string(&path) {
+            Ok(result) => result,
+            Err(err) => err.to_string()
+        }
+    } else {
+        result.error
+    }
+}
+
+#[marine]
+pub fn simple_get_ipfs(ipfs_api: String, cid: String) -> String {
+    let path = vault_path("output");
+    let result = ipfs::get(ipfs_api, cid, &path);
+    if result.success {
+        match std::fs::read_to_string(&path) {
+            Ok(result) => result,
+            Err(err) => err.to_string()
+        }
+    } else {
+        result.error
+    }
+}
+
+fn vault_path(filename: &str) -> String {
+    let cp = marine_rs_sdk::get_call_parameters();
+    format!("/tmp/vault/{}-{}/{}", cp.particle.id, cp.particle.token, filename)
 }
