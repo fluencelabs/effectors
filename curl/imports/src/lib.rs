ef99836e2fcc1ac0bec8353781c7af6e5f473748
Maria Kuklina <maria@fluence.one>
2024-03-05 14:02:58 +0100

    fix interface and sdk

--- curl/imports/src/lib.rs
+++ curl/imports/src/lib.rs
@@ -5,11 +5,11 @@ use marine_rs_sdk::marine;
 #[module_import("curl_effector")]
 extern "C" {
     // Make an HTTP POST request with the request's body taken from the `data_vault_path` file
-    // The reponse is written to the `request.output_data_path` file.
+    // The reponse is written to the `output_vault_path` file.
     // Note that the provided path should be a full path in the Particle Vault.
-    pub fn curl_post(request: CurlRequest, data_vault_path: String) -> CurlResult;
+    pub fn curl_post(request: CurlRequest, data_vault_path: String, output_vault_path: String) -> CurlResult;
 
     // Make an HTTP GET request.
-    // The reponse is written to the `request.output_data_path` file.
-    pub fn curl_get(request: CurlRequest) -> CurlResult;
+    // The reponse is written to the `output_vault_path` file.
+    pub fn curl_get(request: CurlRequest, output_vault_path: String) -> CurlResult;
 }
